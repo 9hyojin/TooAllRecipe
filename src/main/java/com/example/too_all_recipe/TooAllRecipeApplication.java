ae72fabb05daf3f7a858c5a1b9f5dc7a89d75872
9hyojin <koohyojin1205@gmail.com>
2023-01-25 18:09:42 +0900

    FrontEnd html , css

---
+++ src/main/java/com/example/too_all_recipe/TooAllRecipeApplication.java
@@ -0,0 +1,13 @@
+package com.example.too_all_recipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TooAllRecipeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TooAllRecipeApplication.class, args);
+    }
+
+}
