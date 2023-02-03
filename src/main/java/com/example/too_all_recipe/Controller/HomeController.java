95c84d16d2c0c3021d4ff2d9da89b0c90fbf4cfd
9hyojin <koohyojin1205@gmail.com>
2023-02-03 10:27:30 +0900

    add recipes.html and recipes.css

--- src/main/java/com/example/too_all_recipe/Controller/HomeController.java
+++ src/main/java/com/example/too_all_recipe/Controller/HomeController.java
@@ -5,10 +5,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-
-    @GetMapping("/")
+    @GetMapping("/home")
     public String home(){
         return "home";
     }
+
+    @GetMapping("/recipes")
+    public String recipes(){return"recipes";}
+
 
 }
